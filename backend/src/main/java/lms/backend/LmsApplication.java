1d2e5227d9e033229d5dc4f31af65a77dd0114d9
Dörner Virag <mal.dorner.virag@gmail.com>
2022-08-03 11:30:04 +0200

    empty backend runs

---
+++ backend/src/main/java/lms/backend/LmsApplication.java
@@ -0,0 +1,15 @@
+package lms.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LmsApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(LmsApplication.class, args);
+		System.out.println("Leave Management System");
+	}
+
+}
