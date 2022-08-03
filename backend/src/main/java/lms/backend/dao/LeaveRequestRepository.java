420c9e8776cf18ac54528e0b4e0eefe0db6cb8a4
Dörner Virag <mal.dorner.virag@gmail.com>
2022-08-03 11:32:50 +0200

    add api files, pom, app.properties

---
+++ backend/src/main/java/lms/backend/dao/LeaveRequestRepository.java
@@ -0,0 +1,9 @@
+package lms.backend.dao;
+
+import lms.model.LeaveRequest;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface LeaveRequestRepository extends JpaRepository<LeaveRequest, Long> {
+}
---
+++ backend/src/main/java/lms/backend/model/LeaveRequest.java
@@ -0,0 +1,55 @@
+package lms.backend.model;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.sun.istack.NotNull;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+@Table
+public class LeaveRequest {
+    @Id
+    @GeneratedValue(strategy= GenerationType.IDENTITY)
+    private Long id;
+
+    @NotNull
+    private Date startDate;
+
+    @NotNull
+    private Date endDate;
+
+    public LeaveRequest() {
+    }
+
+    public LeaveRequest(@JsonProperty("startDate") Date startDate, @JsonProperty("endDate") Date endDate){
+        this.startDate = startDate;
+        this.endDate = endDate;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Date getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(Date arrival) {
+        this.startDate = arrival;
+    }
+
+    public Date getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(Date departure) {
+        this.endDate = departure;
+    }
+
+}
+
