1249158f2f945948ef818a7412987095adce5178
Dörner Virag <mal.dorner.virag@gmail.com>
2022-08-03 10:51:26 +0200

    redone backend 2

--- backend/src/main/java/LMS/backend/LmsApplication.java
+++ backend/src/main/java/LMS/backend/LmsApplication.java
@@ -1,4 +1,4 @@
-package lms.backend;
+package lms;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- backend/src/test/java/LMS/backend/LmsApplicationTests.java
+++ backend/src/test/java/LMS/backend/LmsApplicationTests.java
@@ -1,4 +1,4 @@
-package lms.backend;
+package lms;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
