10cf7ff24893b0ec3a898c0a31d91a8f921f2322
Dörner Virag <mal.dorner.virag@gmail.com>
2022-08-03 11:35:23 +0200

    delete old backend

--- backend_wrong/src/main/java/lms.backend/dao/LeaveRequestRepository.java
+++
@@ -1,9 +0,0 @@
-package lms.dao;
-
-import lms.model.LeaveRequest;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface LeaveRequestRepository extends JpaRepository<LeaveRequest, Long> {
-}
