14c695ef664c85154aaba4a1acbf0d89bc6e0480
Dörner Virag <mal.dorner.virag@gmail.com>
2022-08-03 09:49:20 +0200

    empty projects

---
+++ backend/src/main/java/LMS/LMSBackendApplication.java
@@ -0,0 +1,15 @@
+package LMS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LMSBackendApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(LMSBackendApplication.class, args);
+		System.out.println("hello");
+	}
+
+}
